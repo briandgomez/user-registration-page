e4116d3aebf9bad14df554ecf636ed5708cf2416
Brian Gomez <briangomez@Brians-MacBook-Pro.local>
2023-10-01 20:27:43 -0500

    create routes to profile and registration page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/registration', pathMatch: 'full' },
+  { path: 'profile', component: ProfilePageComponent },
+  { path: 'registration', component: RegistrationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
