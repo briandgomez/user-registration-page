7872274164b41d3d24f4412e0cf074a3f951557b
Brian Gomez <briangomez@Brians-MacBook-Pro.local>
2023-10-05 22:12:50 -0500

    created tests for get-user-profile service

--- src/app/services/user/get-user-profile.service.spec.ts
+++ src/app/services/user/get-user-profile.service.spec.ts
@@ -1,16 +1,61 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { GetUserProfileService } from './get-user-profile.service';
 
 describe('GetUserProfileService', () => {
-  let service: GetUserProfileService;
+    let service: GetUserProfileService;
+    let httpTestingController: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GetUserProfileService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [GetUserProfileService],
+        });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+        service = TestBed.inject(GetUserProfileService);
+        httpTestingController = TestBed.inject(HttpTestingController);
+    });
+
+    afterEach(() => {
+        httpTestingController.verify();
+    });
+
+    it('should creat get user profile service', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should get user profile data', () => {
+        const mockData = {
+            name: 'Lebron James',
+            email: 'kingjames@google.com',
+            bio: 'King james bio.',
+            img: 'https://tinyurl.com/2p9953zy'
+        };
+        service.getUserProfile().subscribe((data) => {
+            expect(data).toEqual(mockData);
+        });
+
+        const req = httpTestingController.expectOne('https://mocki.io/v1/611a3036-4420-48a5-b8da-9b461853cdd2');
+        expect(req.request.method).toBe('GET');
+
+        req.flush(mockData);
+    });
+
+    it('should handle errors when getting user profile data', () => {
+        const errorMessage = 'Error';
+        service.getUserProfile().subscribe(
+            () => {
+                fail('Expected an error, but got a successful response');
+            },
+            (error) => {
+                expect(error).toBe('Could not get user. Please try again.');
+            }
+        );
+
+        const req = httpTestingController.expectOne('https://mocki.io/v1/611a3036-4420-48a5-b8da-9b461853cdd2');
+        expect(req.request.method).toBe('GET');
+
+        req.error(new ErrorEvent('error', { message: errorMessage }));
+    });
 });
